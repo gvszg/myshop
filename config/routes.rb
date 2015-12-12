62491d73e108d7a57d3bc8f25e07cc126195d958
jamesyu <zetaentranceserious@gmail.com>
2015-12-12 21:50:26 +0800

    Build current car and user can add product to their own cart

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   # products
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    member do
+      post 'add_to_cart'
+    end
+  end
 end
