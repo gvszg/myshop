e2fe37194de92fc3d813b7e6f6305a4203cf977d
jamesyu <zetaentranceserious@gmail.com>
2015-12-15 19:00:45 +0800

    Admin can select the state of an order, add aasm event routes and actions to Admin::OrdersController

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -6,4 +6,16 @@ module OrdersHelper
   def order_state(order)
     t("orders.order_state.#{order.aasm_state}")
   end
+
+  def order_options_for_admin(order)
+    render :partial => "admin/orders/state_option", :locals => { :order => order }
+  end
+
+  def order_paid_state(order)
+    if order.paid?
+      "已付款"
+    else
+      "未付款"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,17 @@ Rails.application.routes.draw do
 
   # admin products
   # admin orders
+  # admin order options
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post 'cancel'
+        post 'ship'
+        post 'shipped'
+        post 'return'
+      end
+    end
   end
 
   # users
